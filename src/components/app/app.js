0ceec5cde41e734ef904f0e2fc4bb43519827672
Jonny Graybill <jon.a.graybill@gmail.com>
2019-09-07 13:23:20 -0700

    Initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from '../header/header.js';
+import Footer from '../footer/footer.js';
+import Counter from '../counter/counter.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Counter />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Get Rekt Scrub</h1>
+    </header>
+  );
+};
+
+export default Header;
